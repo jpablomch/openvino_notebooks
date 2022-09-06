9cecca948278e5cdbd561daa6fe935d25c5e9086
Chaunte W. Lacewell <chaunte.w.lacewell@intel.com>
2022-09-06 09:54:21 -0700

    Moved functions to utils and updated sections accordingly

--- notebooks/303-pytorch-bootstrapnas/bootstrapnas_utils.py
+++ notebooks/303-pytorch-bootstrapnas/bootstrapnas_utils.py
@@ -15,6 +15,7 @@ import os
 
 import torch
 from torch import nn
+import time
 
 __all__ = ["MobileNetV2", "mobilenet_v2_cifar10"]
 
@@ -152,3 +153,156 @@ def mobilenet_v2_cifar10(pretrained=False, progress=True, device="cpu", **kwargs
         )
         model.load_state_dict(state_dict)
     return model
+
+
+### Train Function
+def train_epoch(train_loader, model, device, criterion, optimizer, epoch, compression_ctrl, train_iters=None):
+    batch_time = AverageMeter("Time", ":3.3f")
+    losses = AverageMeter("Loss", ":2.3f")
+    top1 = AverageMeter("Acc@1", ":2.2f")
+    top5 = AverageMeter("Acc@5", ":2.2f")
+    progress = ProgressMeter(
+        len(train_loader), [batch_time, losses, top1, top5], prefix="Epoch:[{}]".format(epoch)
+    )
+
+    # switch to train mode
+    model.train()
+
+    compression_scheduler = compression_ctrl.scheduler
+
+    if train_iters is None:
+        train_iters = len(train_loader)
+
+    end = time.time()
+    for i, (images, target) in enumerate(train_loader):
+
+        compression_scheduler.step()
+
+        images = images.to(device)
+        target = target.to(device)
+
+        # compute output
+        output = model(images)
+        loss = criterion(output, target)
+
+        # measure accuracy and record loss
+        acc1, acc5 = accuracy(output, target, topk=(1, 5))
+        losses.update(loss.item(), images.size(0))
+        top1.update(acc1[0], images.size(0))
+        top5.update(acc5[0], images.size(0))
+
+        # compute gradient and do opt step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        print_frequency = 50
+        if i % print_frequency == 0:
+            progress.display(i)
+
+        if i >= train_iters:
+            break
+
+
+### Validate Function
+def validate(model, device, val_loader, criterion=nn.CrossEntropyLoss()):
+    batch_time = AverageMeter("Time", ":3.3f")
+    losses = AverageMeter("Loss", ":2.3f")
+    top1 = AverageMeter("Acc@1", ":2.2f")
+    top5 = AverageMeter("Acc@5", ":2.2f")
+    progress = ProgressMeter(len(val_loader), [batch_time, losses, top1, top5], prefix="Test: ")
+
+    # switch to evaluate mode
+    model.eval()
+
+    with torch.no_grad():
+        end = time.time()
+        for i, (images, target) in enumerate(val_loader):
+            images = images.to(device)
+            target = target.to(device)
+
+            # compute output
+            output = model(images)
+            loss = criterion(output, target)
+
+            # measure accuracy and record loss
+            acc1, acc5 = accuracy(output, target, topk=(1, 5))
+            losses.update(loss.item(), images.size(0))
+            top1.update(acc1[0], images.size(0))
+            top5.update(acc5[0], images.size(0))
+
+            # measure elapsed time
+            batch_time.update(time.time() - end)
+            end = time.time()
+
+            print_frequency = 10
+            if i % print_frequency == 0:
+                progress.display(i)
+
+        print(" * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}".format(top1=top1, top5=top5))
+    return top1.avg, top5.avg, losses.val 
+
+
+### Helpers Functions
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+
+    def __init__(self, name, fmt=":f"):
+        self.name = name
+        self.fmt = fmt
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+    def __str__(self):
+        fmtstr = "{name} {val" + self.fmt + "} ({avg" + self.fmt + "})"
+        return fmtstr.format(**self.__dict__)
+
+
+class ProgressMeter(object):
+    def __init__(self, num_batches, meters, prefix=""):
+        self.batch_fmtstr = self._get_batch_fmtstr(num_batches)
+        self.meters = meters
+        self.prefix = prefix
+
+    def display(self, batch):
+        entries = [self.prefix + self.batch_fmtstr.format(batch)]
+        entries += [str(meter) for meter in self.meters]
+        print("\t".join(entries))
+
+    def _get_batch_fmtstr(self, num_batches):
+        num_digits = len(str(num_batches // 1))
+        fmt = "{:" + str(num_digits) + "d}"
+        return "[" + fmt + "/" + fmt.format(num_batches) + "]"
+
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the accuracy over the k top predictions for the specified values of k"""
+    with torch.no_grad():
+        maxk = max(topk)
+        batch_size = target.size(0)
+
+        _, pred = output.topk(maxk, 1, True, True)
+        pred = pred.t()
+        correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+        res = []
+        for k in topk:
+            correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
+            res.append(correct_k.mul_(100.0 / batch_size))
+        return res
+
